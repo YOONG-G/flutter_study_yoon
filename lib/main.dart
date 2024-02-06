714128f605589c2294b20f66606ae5bae2969ce3
YOOGN <dbs0669@gmail.com>
2024-02-06 15:29:18 +0900

    Teeeeeeeeeeeeeeeeeest

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '융 Flutter Demo',
+      title: '융 Flutter Demo :)',
       theme: ThemeData(
         // This is the theme of your application.
         //
